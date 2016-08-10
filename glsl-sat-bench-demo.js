bf53cc30d3d14d6559614c7828e3ca1132e10989
realazthat <realazthat@gmail.com>
2016-08-10 17:03:20 -0500

    Fixed up the bench demo.
    - Made EXT_disjoint_timer_query mandatory
    - Displaying `regl.stats.cpuTime`, `regl.stats.gpuTime`
    - semistandardized

--- glsl-sat-bench-demo.js
+++ glsl-sat-bench-demo.js
@@ -1,26 +1,20 @@
 
-const work = require('webworkify');
 const $ = require('jquery-browserify');
 const satbench = require('./glsl-sat-bench.js');
 const μs = require('microseconds');
 
 const regl = require('regl')({
-  extensions: ['OES_texture_float'],
-  optionalExtensions: ['EXT_disjoint_timer_query'],
+  extensions: ['OES_texture_float', 'EXT_disjoint_timer_query'],
+  optionalExtensions: [],
   attributes: {preserveDrawingBuffer: true}
 });
 
-
-
-$(document).ready(function(){
-
-
-
+$(document).ready(function () {
   let $results = $('<table style="width: 100%">').appendTo($('body'));
-  $results.append($('<thead><tr><th>Experiment</th><th>Size</th><th>N</th><th>Total Time</th><th>Time Per</th></tr></thead>'));
+  $results.append($('<thead><tr><th>Experiment</th><th>Size</th><th>N</th><th>Total Time</th><th>Total regl CPU Time</th><th>Total regl GPU Time</th><th>Time Per</th></tr></thead>'));
   let $tbody = $('<tbody>').appendTo($results);
 
-  setTimeout(function(){
+  setTimeout(function () {
     let experimentIndex = 1;
     let size = 256;
     let N = 64;
@@ -34,8 +28,9 @@ $(document).ready(function(){
                 .append($('<td>').text(`${size}`).css('padding', '2em'))
                 .append($('<td>').text(`${N}`).css('padding', '2em'))
                 .append($('<td>').text(μs.parse(results.microseconds.total).toString()).css('padding', '2em'))
+                .append($('<td>').text(μs.parse(1000 * results.cpuTime).toString()).css('padding', '2em'))
+                .append($('<td>').text(μs.parse(1000 * results.gpuTime).toString()).css('padding', '2em'))
                 .append($('<td>').text(timePer).css('padding', '2em'));
-    $tr.appendTo($results);
+    $tr.appendTo($tbody);
   });
-
 });
--- glsl-sat-bench.js
+++ glsl-sat-bench.js
@@ -1,33 +1,32 @@
 
-const quad = require('glsl-quad');
 const computeSat = require('./glsl-sat.js').computeSat;
 const μs = require('microseconds');
 const range = require('array-range');
 
-function benchSatCompute({regl, size, textureData = null, N, components = 'rgba', type = 'vec4'}) {
+function benchSatCompute ({regl, size, textureData = null, N, components = 'rgba', type = 'vec4'}) {
   let fbos = range(2).map(function () {
-      return regl.framebuffer({
-        color: regl.texture({
-          width: size,
-          height: size,
-          format: 'rgba',
-          type: 'float',
-          depth: false,
-          stencil: false,
-          wrap: 'clamp',
-          mag: 'nearest',
-          min: 'nearest'
-        }),
+    return regl.framebuffer({
+      color: regl.texture({
+        width: size,
+        height: size,
+        format: 'rgba',
+        type: 'float',
         depth: false,
         stencil: false,
-        depthStencil: false,
-        depthTexture: false,
         wrap: 'clamp',
         mag: 'nearest',
         min: 'nearest'
-      });
+      }),
+      depth: false,
+      stencil: false,
+      depthStencil: false,
+      depthTexture: false,
+      wrap: 'clamp',
+      mag: 'nearest',
+      min: 'nearest'
     });
-  
+  });
+
   let outFbo = regl.framebuffer({
     color: regl.texture({
       width: size,
@@ -50,9 +49,9 @@ function benchSatCompute({regl, size, textureData = null, N, components = 'rgba'
   });
 
   if (textureData === null || textureData === undefined) {
-    textureData = new Uint8Array(size*size*4);
+    textureData = new Uint8Array(size * size * 4);
   }
-  
+
   let texture = regl.texture({
     data: textureData,
     width: size,
@@ -73,15 +72,24 @@ function benchSatCompute({regl, size, textureData = null, N, components = 'rgba'
     }
   };
 
+  let scope = regl({
+    profile: true
+  });
+
   let t0 = μs.now();
   for (let i = 0; i < N; ++i) {
     let samplet0 = μs.now();
 
-    computeSat({texture: texture, fbos: fbos, outFbo: outFbo, regl, components, type});
+    scope(() => {
+      computeSat({texture: texture, fbos: fbos, outFbo: outFbo, regl, components, type});
+    });
 
     results.microseconds.samples.push(μs.since(samplet0));
   }
-  
+
+  results.cpuTime = scope.stats.cpuTime;
+  results.gpuTime = scope.stats.gpuTime;
+
   results.microseconds.total = μs.since(t0);
 
   return results;
